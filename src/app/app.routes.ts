251a9cf9fe03d261e7ee9f6a24a3987944c33c61
willian ruiz <122157786+willianRuizCSS@users.noreply.github.com>
2024-10-11 22:16:01 -0500

    fon awsome , iconos boostrap y google fonds

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/listar-producto/listar-producto.component').then(m => m.ListarProductoComponent)
+  }, {
+    path: 'crear-producto',
+    loadComponent: () => import('./components/crear-producto/crear-producto.component').then(m => m.CrearProductoComponent)
+  },
+  {
+    path: 'editar-producto/:id',
+    loadComponent: () => import('./components/crear-producto/crear-producto.component').then(m => m.CrearProductoComponent)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
