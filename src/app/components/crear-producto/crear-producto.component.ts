2181c108caa88ed1ccfb8fcb19e08e3ab1979b66
willian ruiz <122157786+willianRuizCSS@users.noreply.github.com>
2024-10-12 17:37:41 -0500

    toast

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { Producto } from '../../models/producto';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-crear-producto',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule,BrowserAnimationsModule, ToastrModule],
   templateUrl: './crear-producto.component.html',
   styleUrl: './crear-producto.component.css'
 })
@@ -14,7 +18,10 @@ export class CrearProductoComponent implements OnInit {
 
   productoForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+    private router:Router,
+    private toastr: ToastrService
+  ) {
     this.productoForm = this.fb.group({
       producto: ['', Validators.required],
       categoria: ['', Validators.required],
@@ -28,7 +35,17 @@ export class CrearProductoComponent implements OnInit {
   }
 
   agregarProducto() {
-  console.log(this.productoForm.value);
+    console.log(this.productoForm.value);
+    const PRODUCTO: Producto = {
+      nombre: this.productoForm.get('producto')?.value,
+      categoria: this.productoForm.get('categoria')?.value,
+      ubicacion: this.productoForm.get('ubicacion')?.value,
+      precio: this.productoForm.get('precio')?.value
+    }
+
+    console.log(PRODUCTO);
+    this.toastr.success('Producto registrado correctamente!', 'Producto registrado!');
+    this.router.navigate(['/']);
   }
 
 }
--- src/app/components/listar-producto/listar-producto.component.ts
+++ src/app/components/listar-producto/listar-producto.component.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterLink } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
 
 @Component({
   selector: 'app-listar-producto',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule,BrowserAnimationsModule, ToastrModule],
   templateUrl: './listar-producto.component.html',
   styleUrl: './listar-producto.component.css'
 })
