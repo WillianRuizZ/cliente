1d44a6b48fd32883c2e5dcf92276c7219f6dc566
willian ruiz <122157786+willianRuizCSS@users.noreply.github.com>
2024-10-12 16:29:03 -0500

    reactive forms

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-crear-producto',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './crear-producto.component.html',
   styleUrl: './crear-producto.component.css'
 })
-export class CrearProductoComponent {
+export class CrearProductoComponent implements OnInit {
+
+
+  productoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required],
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  agregarProducto() {
+  console.log(this.productoForm.value);
+  }
 
 }
--- src/app/components/listar-producto/listar-producto.component.ts
+++ src/app/components/listar-producto/listar-producto.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listar-producto',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './listar-producto.component.html',
   styleUrl: './listar-producto.component.css'
 })
