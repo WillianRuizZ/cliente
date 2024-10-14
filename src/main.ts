46ec2e49ff6209cd8b5a4bfa8e33915401581b21
willian ruiz <122157786+willianRuizCSS@users.noreply.github.com>
2024-10-14 13:28:28 -0500

    provideHttpClient()

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes'; // Asegúrate de que la ruta sea correcta
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes), // Proveer las rutas de la aplicación
     importProvidersFrom(BrowserAnimationsModule),
-    importProvidersFrom(ToastrModule.forRoot())
+    importProvidersFrom(ToastrModule.forRoot()),
+    provideHttpClient() // Aquí se configura HttpClient para toda la app, NO necesita anidación dentro de otro array.
   ]
 })
-  .catch((err) => console.error(err));
+.catch((err) => console.error(err));
